c8f60280e9aa9d0062f3201f39bf5791f5055e43
kingDaniel2004 <danielelvisr@gmail.com>
2021-05-30 13:23:49 -0400

    update picom

--- nixOS/system/configuration.nix
+++ nixOS/system/configuration.nix
@@ -13,7 +13,7 @@
   # boot.loader.systemd-boot.enable = true;
   # boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub = {
- 	enable = true;
+ 	      enable = true;
         devices = [ "nodev" ];
         efiInstallAsRemovable = true;
         efiSupport = true;
--- nixOS/users/dani/home.nix
+++ nixOS/users/dani/home.nix
@@ -20,17 +20,6 @@ let
   unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
 
 
-
-   ##### overridding picom
-  package = pkgs.picom.overrideAttrs(o: {
-    src = pkgs.fetchFromGitHub {
-      owner = "jonaburg"; # github user name
-      repo = "picom"; # github repo name
-      rev = "e553e00f48de67d52fe75de9e0e940d85aa14a24"; # an id of a commit
-      sha256 = "04svbv7v73q8yn9la69451rda6l2pgxcphv2zlkdqaxxdbp69195"; # got it from -> nix-prefetch-url --unpack <github .tar url>
-    };
-  });
-
 in
 
 {
@@ -56,8 +45,6 @@ in
 
 
   home.packages = with pkgs; [
-    picom   ######### added picom
-
 
 
     # browser
@@ -86,6 +73,7 @@ in
     notify-desktop
     jgmenu
     unstable.deadd-notification-center
+    picom   ######### added picom
 
     # other
     discord
@@ -187,14 +175,26 @@ in
 
 
 
- # nvim 0.5
+  # overlays
   nixpkgs.overlays = [
+
+     # nvim 0.5
     (import (builtins.fetchTarball {
       url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
     }))
+
+     # picom - jonaburg
+    (self: super: {
+      picom = super.picom.overrideAttrs (old: {
+        src = super.fetchFromGitHub {
+          owner = "jonaburg";
+          repo = "picom";
+          rev = "a8445684fe18946604848efb73ace9457b29bf80";
+          sha256 = "154s67p3lxdv9is3lnc32j48p7v9n18ga1j8ln1dxcnb38c19rj7";
+        };
+      });
+    })
   ];
-
-
 
 
 
