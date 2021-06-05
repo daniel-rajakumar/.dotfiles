4f88c46650a5482ef2ae7dc2cb5d6ab07a373370
kingDaniel2004 <danielelvisr@gmail.com>
2021-06-05 16:41:26 -0400

    added startups

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+ #!/bin/sh
+
 
 apps=(
         "Outlook \0icon\x1fms-outlook"
@@ -6,31 +7,32 @@ apps=(
         "Youtube Music \0icon\x1fyoutube-music-desktop-app"
 )
 
-
 for app in "${apps[@]}"; do
     list+="$app\n"
 done
 
-
-app="$(echo -en $list | rofi -dmenu -icon-theme "Papirus" -show-icons)"
-
-
-if [[ "$app" == *"Outlook"* ]]; then
-        brave --kiosk --new-window "https://outlook.com" &
-        sleep 1.5
-        bspc node focused -t tiled
-        bspc node -d 10
-
-elif [[ "$app" == *"Office"* ]]; then
-        brave --kiosk --new-window "https://office.com" &
-        sleep 1.5
-        bspc node focused -t tiled
-        bspc node -d 19
-
-elif [[ "$app" == *"Youtube Music"* ]]; then
-        brave --kiosk --new-window "https://music.youtube.com" &
-        sleep 1.5
-        bspc node focused -t tiled
-        bspc node -d 0
+if [ -z "$@" ]; then
+   app="$(echo -en $list | rofi -dmenu -icon-theme "Papirus" -show-icons -p "websites")"
+else
+   app="$@"
 fi
+
+        if [[ "$app" == *"Outlook"* ]]; then
+                brave --kiosk --new-window "https://outlook.com" &
+                sleep 1.5
+                bspc node focused -t tiled
+                bspc node -d 10
+
+        elif [[ "$app" == *"Office"* ]]; then
+                brave --kiosk --new-window "https://office.com" &
+                sleep 1.5
+                bspc node focused -t tiled
+                bspc node -d 19
+
+        elif [[ "$app" == *"Youtube Music"* ]]; then
+                brave --kiosk --new-window "https://music.youtube.com" &
+                sleep 1.5
+                bspc node focused -t tiled
+                bspc node -d 0
+        fi
 
